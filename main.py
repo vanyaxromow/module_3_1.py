d274f14cd75b36f98d3450c8e8c52657e6215bee
vanyaxromow <vanya.xromow@yandex.ru>
2024-07-07 14:45:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    my_tuple = len(string), string.upper(), string.lower()
+    return my_tuple
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.upper() == string.upper():
+            a = True
+        else:
+            a = False
+    return a
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+
